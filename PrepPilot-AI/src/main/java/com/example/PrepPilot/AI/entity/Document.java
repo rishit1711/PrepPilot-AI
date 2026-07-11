d74eeb12cf05498a07499e12c47e525ec7d700fc
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-11 14:34:57 +0530

    Database Schema Design

--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/Document.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/Document.java
@@ -1,10 +1,54 @@
 package com.example.PrepPilot.AI.entity;
 
+import com.example.PrepPilot.AI.entity.enums.DocType;
+import com.example.PrepPilot.AI.entity.enums.UploadStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Entity;
+import java.time.Instant;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "documents")
 public class Document {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(nullable = false)
+    private String originalFileName;
+
+    @Column(nullable = false)
+    private String storedFileName;
+
+    @Column(nullable = false)
+    private String storagePath;
+
+    @Column(nullable = false)
+    private String mimeType;
+
+    @Column(nullable = false)
+    private Long fileSize;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DocType docType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UploadStatus uploadStatus;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Instant uploadedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/User.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/User.java
@@ -4,9 +4,12 @@ import com.example.PrepPilot.AI.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -37,6 +40,13 @@ public class User {
             fetch = FetchType.LAZY)
     private UserProfile userProfile;
 
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY
+    )
+    private List<Document> documents = new ArrayList<>();
     @CreationTimestamp
     @Column(updatable = false)
     private Instant createdAt;
