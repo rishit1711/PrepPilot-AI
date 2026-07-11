d81bb2076767520a222660ad36805ad453eab4cf
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-11 14:39:29 +0530

    Database Schema Design

--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/User.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/User.java
@@ -4,7 +4,6 @@ import com.example.PrepPilot.AI.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
