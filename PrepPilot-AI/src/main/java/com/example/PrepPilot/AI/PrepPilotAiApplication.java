d6a5d99fc098216495f2b04b46c8df9748393180
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-11 10:52:15 +0530

    Initial Spring Boot project

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/PrepPilotAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrepPilot.AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepPilotAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepPilotAiApplication.class, args);
+	}
+
+}
