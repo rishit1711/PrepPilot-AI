fc3470f58478c5c1659068c7c304024707f2b5b4
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-17 19:18:12 +0530

    Document Module Started

--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/DocumentRepository.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/DocumentRepository.java
@@ -1,6 +1,14 @@
 package com.example.PrepPilot.AI.repository;
 
+import com.example.PrepPilot.AI.entity.Document;
+import com.example.PrepPilot.AI.entity.User;
+import com.example.PrepPilot.AI.entity.enums.DocumentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DocumentRepository extends JpaRepository {
+import java.util.List;
+
+public interface DocumentRepository extends JpaRepository<Document,Long> {
+
+    List<Document> findByUser(User user);
+    List<Document> findByUserAndDocumentType(DocumentType documentType);
 }
---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/service/StorageService.java
@@ -0,0 +1,12 @@
+package com.example.PrepPilot.AI.service;
+
+import com.example.PrepPilot.AI.entity.enums.DocumentType;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+    String store(MultipartFile file, DocumentType documentType);
+    Resource load(String storagePath);
+
+    void delete(String storagePath);
+}
