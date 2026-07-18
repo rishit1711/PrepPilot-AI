f6d329b6927323c6506950b7656aa6bc23193138
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-18 11:55:09 +0530

    Document Related Exceptions

--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/advices/GlobalExceptionHandler.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/advices/GlobalExceptionHandler.java
@@ -111,7 +111,4 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
-
-
-
 }
--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/controller/DocumentController.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/controller/DocumentController.java
@@ -1,8 +1,29 @@
 package com.example.PrepPilot.AI.controller;
 
+import com.example.PrepPilot.AI.dto.UploadResponse;
+import com.example.PrepPilot.AI.entity.enums.DocumentType;
+import com.example.PrepPilot.AI.service.DocumentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class DocumentController {
+    private final DocumentService documentService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<UploadResponse> docUpload(
+            @RequestParam("file")MultipartFile file,
+            @RequestParam("documentType")DocumentType documentType){
+
+        UploadResponse response = documentService.Upload(file,documentType);
+        return ResponseEntity.ok(response);
+    }
 
 }
