e70ce6a2975fcf5ae253e5829b7cc8f2dcd49348
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-21 11:42:29 +0530

    Pdf to text Extraction

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/parser/pdfExtractionService.java
@@ -0,0 +1,23 @@
+package com.example.PrepPilot.AI.parser;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class pdfExtractionService {
+
+    public String extractText(MultipartFile file) throws IOException {
+
+        try (PDDocument document = Loader.loadPDF(file.getBytes())) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(document);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
