58070e71d7fbe33c9c84a28e5ac74fd1e4fa3514
Rishit Roy <hellorishit.roy@gmail.com>
2026-07-22 09:52:30 +0530

    Setting up chunking service

--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/RagModule/pdfExtractionService.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/RagModule/pdfExtractionService.java
@@ -6,6 +6,7 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import java.io.IOException;
 
 @Service
