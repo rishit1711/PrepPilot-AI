1ce627837dc220bd4ac144063193df71c78dbbb7
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-07 14:21:26 +0530

    JDAnalysis Db Storage

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/mapper/JDMapper.java
@@ -0,0 +1,11 @@
+package com.example.PrepPilot.AI.mapper;
+
+import com.example.PrepPilot.AI.dto.JDAnalysisResponse;
+import com.example.PrepPilot.AI.entity.JDMatchAnalysis;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface JDMapper {
+
+    JDMatchAnalysis toEntity(JDAnalysisResponse response);
+}
---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/JDAnalysisRepository.java
@@ -0,0 +1,9 @@
+package com.example.PrepPilot.AI.repository;
+
+import com.example.PrepPilot.AI.entity.JDMatchAnalysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JDAnalysisRepository extends JpaRepository<JDMatchAnalysis,Long> {
+}
