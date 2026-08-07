d7270403e389fe352070ed811c133b1ba7388fdb
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-07 18:27:38 +0530

    InterviewBluePrint Started

--- PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/controller/InterviewBluePrintController.java
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/controller/InterviewBluePrintController.java
@@ -1,20 +1,27 @@
 package com.example.PrepPilot.AI.controller;
 
+import com.example.PrepPilot.AI.dto.BluePrintRequest;
 import com.example.PrepPilot.AI.dto.BluePrintResponse;
+import com.example.PrepPilot.AI.service.BluePrintService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class QuestionBluePrintController {
+public class InterviewBluePrintController {
+    private final BluePrintService bluePrintService;
 
     // Kam hai to just give a clear overview of what section to be targetted in interview
 
     @PostMapping(path = "/blueprint")
-    public ResponseEntity<BluePrintResponse> blueprint()
+    public ResponseEntity<BluePrintResponse> blueprint(@RequestBody BluePrintRequest request){
+        return ResponseEntity.ok(bluePrintService.GenerateBluePrint(request));
+
+    }
 
 }
