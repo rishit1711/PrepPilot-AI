8dcbdbdcd845ce50634a4c407018b27e97f48c22
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-08 13:21:11 +0530

    Storing Resume Metadata

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/ResumeAnalysisRepository.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.repository;
+
+import com.example.PrepPilot.AI.entity.ResumeAnalysis;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeAnalysisRepository extends JpaRepository<ResumeAnalysis,Long> {
+}
