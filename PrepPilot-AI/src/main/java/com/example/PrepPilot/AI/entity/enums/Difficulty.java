60f101bfa38ef457fe2a7574a40e1f63f7bbaf4f
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-15 13:09:29 +0530

    Started With Interview BluePrint

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/enums/Difficulty.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.entity.enums;
+
+public enum Difficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/entity/enums/Priority.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.entity.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
