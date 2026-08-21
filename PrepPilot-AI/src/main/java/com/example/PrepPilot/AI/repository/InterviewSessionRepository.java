cdbe8319acbe7ed7da19a73634acbd6fe086f585
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-21 23:35:27 +0530

    Adaptive Interview Entity Module

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/InterviewSessionRepository.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.repository;
+
+import com.example.PrepPilot.AI.entity.InterviewSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewSessionRepository extends JpaRepository<InterviewSession,Long> {
+}
