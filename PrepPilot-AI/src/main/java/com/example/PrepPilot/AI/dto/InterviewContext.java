b6b24d94ec38cbed6ccdb507d86502779882ed33
Rishit Roy <hellorishit.roy@gmail.com>
2026-08-23 00:16:18 +0530

    Adaptive Interview Implementation

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/dto/InterviewContext.java
@@ -0,0 +1,15 @@
+package com.example.PrepPilot.AI.dto;
+
+import com.example.PrepPilot.AI.entity.InterviewBluePrint;
+import com.example.PrepPilot.AI.entity.InterviewSession;
+import com.example.PrepPilot.AI.entity.JDMatchAnalysis;
+import com.example.PrepPilot.AI.entity.ResumeAnalysis;
+import lombok.Builder;
+
+@Builder
+public class InterviewContext {
+    private ResumeAnalysis resumeAnalysis;
+    private JDMatchAnalysis jdMatchAnalysis;
+    private InterviewBluePrint interviewBluePrint;
+    private InterviewSession interviewSession;
+}
