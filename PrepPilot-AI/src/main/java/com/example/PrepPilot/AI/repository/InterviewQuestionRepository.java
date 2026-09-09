819448bc25531162173cb64550e819537142d5a5
Rishit Roy <hellorishit.roy@gmail.com>
2026-09-09 19:29:42 +0530

    Interview Question Generate

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/InterviewQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.repository;
+
+import com.example.PrepPilot.AI.entity.InterviewQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestion,Long> {
+}
