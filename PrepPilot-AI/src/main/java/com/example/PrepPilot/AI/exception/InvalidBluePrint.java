7a7fc683f3a09cc55c84caeb10d5527fafe88850
Rishit Roy <hellorishit.roy@gmail.com>
2026-09-09 19:39:30 +0530

    Start Interview Fix

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/exception/InvalidBluePrint.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.exception;
+
+public class InvalidBluePrint extends RuntimeException {
+    public InvalidBluePrint(String message) {
+        super(message);
+    }
+}
