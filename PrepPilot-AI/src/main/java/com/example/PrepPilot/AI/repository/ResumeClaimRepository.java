14a3be8c82fd9ff8ad37a17da6f60b2bc109ae88
Rishit Roy <hellorishit.roy@gmail.com>
2026-09-23 21:42:00 +0530

    Resume Claim to DB

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/ResumeClaimRepository.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.repository;
+
+import com.example.PrepPilot.AI.entity.ResumeClaim;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeClaimRepository extends JpaRepository<ResumeClaim,Long> {
+}
