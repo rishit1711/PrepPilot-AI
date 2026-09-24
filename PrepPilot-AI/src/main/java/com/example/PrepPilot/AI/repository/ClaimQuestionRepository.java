1598ddd5ebec198f02f99de725372970017c0c37
Rishit Roy <hellorishit.roy@gmail.com>
2026-09-24 15:45:27 +0530

    Resume Claims Question Generation

---
+++ PrepPilot-AI/src/main/java/com/example/PrepPilot/AI/repository/ClaimQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.example.PrepPilot.AI.repository;
+
+import com.example.PrepPilot.AI.entity.ClaimQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClaimQuestionRepository extends JpaRepository<ClaimQuestion,Long> {
+}
